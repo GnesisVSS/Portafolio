bda2d4d497b4e43e37594a7aff1194a99f972034
GenesisVSS <gnesis.vss@gmail.com>
2022-02-04 17:22:55 -0300

    BD

--- Web/NoMásAccidentes/Inicio/tests.py
+++
@@ -1,6 +0,0 @@
-from django.http import request
-from django.test import TestCase
-
-from Inicio.views import cerrar_sesion
-
-# Create your tests here.
--- Web/NoMásAccidentes/Inicio/webservice.py
+++
@@ -1,8 +0,0 @@
-from zeep import Client
- 
-import json
-
-cliente = Client('http://localhost:61887/Service1.svc?wsdl')
-
-prueba=cliente.service.datosprf("STEVEN")
-print(prueba)
